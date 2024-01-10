2cbe428bbe79bfef8da59783a4183122e160b051
Arpit-2802 <arpitthakur2802@gmail.com>
2024-01-10 17:11:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Header from './components/Header'
+import UserInput from './components/UserInput';
+import {useState} from 'react';
+import Results from './components/Results';
+
+function App() {
+
+  const[userInput,setUserInput]=useState({
+    initialInvestment:10000,
+    annualInvestment:1200,
+    expectedReturn:6,
+    duration:10
+  })
+
+  function handleInput(inputIdentifier,newValue){
+    setUserInput(prevUserInput=>{
+        return{
+            ...prevUserInput,
+            [inputIdentifier]: +newValue
+        }
+    });
+  }
+
+
+  return (
+    <>
+      <Header />
+      <UserInput onChange={handleInput} userInput={userInput} />
+      <Results inputs={userInput}/>
+    </>
+  )
+}
+
+export default App
